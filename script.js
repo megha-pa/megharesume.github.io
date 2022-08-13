6573991397c040f703b6e634a43eeebcd67a23c3
megha-pa <86820581+megha-pa@users.noreply.github.com>
2022-08-13 17:32:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Data Scientist","MLOPS Engineer","Data Analyst","ML Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Data Scientist","MLOPS Engineer","Data Analyst","ML Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
